29a9690b650b8266193e94a932d19e41e093aa54
Akihiro Tanaka <akihiro.fin@icloud.com>
2021-08-16 14:43:11 +0300

    Fixed rendering likes

--- likes.py
+++ likes.py
@@ -2,7 +2,7 @@ from db import db
 
 
 def get_list(thread_id):
-    sql = """SELECT L.message_id, COUNT(*) FROM likes L, messages M, threads T 
-    WHERE L.message_id=M.id AND M.thread_id=T.id AND M.visible=true AND T.visible=true AND L.bool=true AND T.id=:thread_id GROUP BY L.message_id ORDER BY L.message_id"""
+    sql = """SELECT M.id as message_id, COUNT(L.bool) FROM messages M LEFT JOIN threads T ON M.thread_id=T.id LEFT JOIN likes L ON L.message_id=M.id 
+    WHERE M.visible=true AND T.visible=true AND T.id=:thread_id GROUP BY M.id ORDER BY M.id"""
     result = db.session.execute(sql, {"thread_id":thread_id})
     return result.fetchall()
